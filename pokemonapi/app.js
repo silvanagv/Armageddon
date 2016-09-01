ed3fed06612f1d38abcb68d84a859e998a29b415
ipc103 <iancandysemail@gmail.com>
2016-09-01 09:44:34 -0400

    remove solution

--- pokemonapi/app.js
+++ pokemonapi/app.js
@@ -1,17 +1,1 @@
 console.log("App.js is running");
-
-$('button').click(function(){
-  var pokeName = $('input').val().toLowerCase();
-  $.ajax({
-    url: 'http://pokeapi.co/api/v2/pokemon/'+pokeName,
-    method: 'GET',
-    success: function(response){
-      console.log('JSON object from server', response);
-      var name = response.name,
-          fd   = response.sprites.front_default;
-
-      $('.result #pokemonName').text(name);
-      $('.result #pokemonFrontDefault').attr('src', fd);
-    }
-  })
-})
